a65f5d8b905fae5c80de6ce0d7dab8b58e120dc1
DigvijayDheer <digvijaydheersaini@gmail.com>
2022-12-22 18:02:55 +0530

    add axios method to

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,119 @@
+// const baseUrl = "https://jsonplaceholder.typicode.com/posts";
+
+// const listElement = document.querySelector(".posts");
+// const postTemplate = document.getElementById("single-post");
+// const form = document.querySelector("#new-post form");
+// const fetchButton = document.querySelector("#available-posts button");
+// const postList = document.querySelector("ul");
+
+// // USING XMLHttpRequest
+
+// // function sendHttpRequest(method, url, data) {
+// // const promise = new Promise((resolve, reject) => {
+// // const xhr = new XMLHttpRequest();
+
+// // xhr.open(method, url);
+
+// // xhr.responseType = "json";
+
+// // xhr.onload = function () {
+// // 	if (xhr.status >= 200 && xhr.status < 300) {
+// // 		resolve(xhr.response);
+// // 	} else {
+// // 		reject(new Error("Something went wrong!"));
+// // 	}
+// // };
+
+// // xhr.onerror = function () {
+// // 	reject(new Error("Failed to send request!"));
+// // };
+
+// // xhr.send(JSON.stringify(data));
+
+// // });
+
+// // return promise;
+// // }
+
+// // USING FETCH
+// function sendHttpRequest(method, url, data) {
+// 	return fetch(url, {
+// 		method: method,
+// 		body: JSON.stringify(data),
+// 		// body: data,
+// 		headers: {
+// 			"Content-Type": "application/json",
+// 		},
+// 	})
+// 		.then((response) => {
+// 			if (response.status >= 200 && response.status < 300) {
+// 				return response.json();
+// 			} else {
+// 				return response.json().then((errData) => {
+// 					console.log(errData);
+// 					throw new Error("Something went wrong on server-side!");
+// 				});
+// 			}
+// 		})
+// 		.catch((error) => {
+// 			console.log(error);
+// 			throw new Error("Something went wrong!");
+// 		});
+// }
+
+// async function fetchPosts() {
+// 	try {
+// 		const responseData = await sendHttpRequest("GET", baseUrl);
+
+// 		const listOfPosts = responseData;
+
+// 		for (const post of listOfPosts) {
+// 			const postEl = document.importNode(postTemplate.content, true);
+// 			postEl.querySelector("h2").textContent = post.title.toUpperCase();
+// 			postEl.querySelector("p").textContent = post.body;
+// 			postEl.querySelector("li").id = post.id;
+// 			listElement.append(postEl);
+// 		}
+// 	} catch (error) {
+// 		alert(error.message);
+// 	}
+// }
+
+// async function createPost(title, content) {
+// 	const userId = Math.random();
+
+// 	const post = {
+// 		title: title,
+// 		body: content,
+// 		userId: userId,
+// 	};
+
+// 	// const fd = new FormData();
+// 	// fd.append("title", title);
+// 	// fd.append("body", content);
+// 	// fd.append("userId", userId);
+
+// 	sendHttpRequest("POST", baseUrl, post);
+// }
+
+// fetchButton.addEventListener("click", fetchPosts);
+// form.addEventListener("submit", (event) => {
+// 	event.preventDefault();
+// 	const enteredTitle = event.currentTarget.querySelector("#title").value;
+// 	const enteredContent = event.currentTarget.querySelector("#content").value;
+
+// 	createPost(enteredTitle, enteredContent);
+// });
+
+// postList.addEventListener("click", (event) => {
+// 	if (event.target.tagName === "BUTTON") {
+// 		const postId = event.target.closest("li").id;
+// 		sendHttpRequest("DELETE", baseUrl + "/" + postId);
+// 	}
+// });
+
+// USING AXIOS LIBRARY
+
 const baseUrl = "https://jsonplaceholder.typicode.com/posts";
 
 const listElement = document.querySelector(".posts");
@@ -6,61 +122,11 @@ const form = document.querySelector("#new-post form");
 const fetchButton = document.querySelector("#available-posts button");
 const postList = document.querySelector("ul");
 
-function sendHttpRequest(method, url, data) {
-	// const promise = new Promise((resolve, reject) => {
-	// const xhr = new XMLHttpRequest();
-
-	// xhr.open(method, url);
-
-	// xhr.responseType = "json";
-
-	// xhr.onload = function () {
-	// 	if (xhr.status >= 200 && xhr.status < 300) {
-	// 		resolve(xhr.response);
-	// 	} else {
-	// 		reject(new Error("Something went wrong!"));
-	// 	}
-	// };
-
-	// xhr.onerror = function () {
-	// 	reject(new Error("Failed to send request!"));
-	// };
-
-	// xhr.send(JSON.stringify(data));
-
-	// });
-
-	// return promise;
-
-	return fetch(url, {
-		method: method,
-		body: JSON.stringify(data),
-		// body: data,
-		headers: {
-			"Content-Type": "application/json",
-		},
-	})
-		.then((response) => {
-			if (response.status >= 200 && response.status < 300) {
-				return response.json();
-			} else {
-				return response.json().then((errData) => {
-					console.log(errData);
-					throw new Error("Something went wrong on server-side!");
-				});
-			}
-		})
-		.catch((error) => {
-			console.log(error);
-			throw new Error("Something went wrong!");
-		});
-}
-
 async function fetchPosts() {
 	try {
-		const responseData = await sendHttpRequest("GET", baseUrl);
+		const response = await axios.get(baseUrl);
 
-		const listOfPosts = responseData;
+		const listOfPosts = response.data;
 
 		for (const post of listOfPosts) {
 			const postEl = document.importNode(postTemplate.content, true);
@@ -71,6 +137,7 @@ async function fetchPosts() {
 		}
 	} catch (error) {
 		alert(error.message);
+		console.log(error.response);
 	}
 }
 
@@ -83,15 +150,11 @@ async function createPost(title, content) {
 		userId: userId,
 	};
 
-	// const fd = new FormData();
-	// fd.append("title", title);
-	// fd.append("body", content);
-	// fd.append("userId", userId);
-
-	sendHttpRequest("POST", baseUrl, post);
+	await axios.post(baseUrl, post);
 }
 
 fetchButton.addEventListener("click", fetchPosts);
+
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
 	const enteredTitle = event.currentTarget.querySelector("#title").value;
@@ -103,6 +166,6 @@ form.addEventListener("submit", (event) => {
 postList.addEventListener("click", (event) => {
 	if (event.target.tagName === "BUTTON") {
 		const postId = event.target.closest("li").id;
-		sendHttpRequest("DELETE", baseUrl + "/" + postId);
+		axios.delete(baseUrl + "/" + postId);
 	}
 });
